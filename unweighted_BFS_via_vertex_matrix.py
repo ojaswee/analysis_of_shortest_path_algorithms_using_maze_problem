d0c4b73f44a98cc58699079558bcb442fe2b79e5
ojaswee <ojaswee@outlook.com>
2018-04-11 19:06:25 -0500

    added BFS code

--- unweighted_BFS_via_vertex_matrix.py
+++ unweighted_BFS_via_vertex_matrix.py
@@ -4,12 +4,12 @@ Here I am trying to solve BFS via vertices matrix.
 
 When popping the stack pop from the front not from the back
 because we want to visit the node in ascending order
-For example 1 has children 6 and 21 then we want to go to
+For example if 1 has children 6 and 21 then we want to go to
 6 before 21.
 '''
 import numpy as np
 # get graph from csv file
-graphx = np.loadtxt("vertices_matrix.csv", dtype='i', delimiter=',')
+graphx = np.loadtxt("unweighted_undirected_vertex_matrix.csv", dtype='i', delimiter=',')
 # print(graphx)
 
 def bfs_adjacency_mat(G, start, goal,count):
@@ -18,7 +18,7 @@ def bfs_adjacency_mat(G, start, goal,count):
     stack.append(start)
     goal = goal -1
     while len(stack)!=0:
-        # pop the first element not from the end of the stack
+        # pop the first element not from the end of the list
         v=stack.pop(0)
         if visited[v]==False:
             visited[v]=True
@@ -34,10 +34,29 @@ def bfs_adjacency_mat(G, start, goal,count):
                     break
     return count
 
-# def bfs_adjacency_mat(G, start, goal,count):
-#     print G
+# def dfs_adjacency_mat(G, start, goal,count):
+#     visited = [False] * len(G[0])
+#     stack = []
+#     stack.append(start)
+#     goal = goal - 1
+#     while len(stack) != 0:
+#         # pop the first element not from the end of the list
+#         v = stack.pop()
+#         if visited[v] == False:
+#             visited[v] = True
+#             count += 1
+#             print(v + 1),
+#             w = 0
+#             while (w <len(G[0])):
+#                 if G[v][w] > 0:
+#                     stack.append(v)
+#                     stack.append(w)
+#                     w+1;
+#                     print w
+#                 return count
 
 #Entry point for program execution
 count = bfs_adjacency_mat(graphx, 0, 10,0)
+# count = dfs_adjacency_mat(graphx, 0, 10,0)
 print " "
 print "Total nodes visited:",count
